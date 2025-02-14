b62d150a899bbccbd755548ee80639d04fdcb7cc
Sandhep <b.sandhep111@gmail.com>
2025-02-14 17:28:12 +0530

    created auth state manager

---
+++ src/slices/authSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  authToken: localStorage.getItem("authToken") || null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.authToken = action.payload;
+      localStorage.setItem("authToken", action.payload);
+    },
+    logout: (state) => {
+      state.authToken = null;
+      localStorage.removeItem("authToken");
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
