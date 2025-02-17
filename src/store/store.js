9e43db39f5117f26bfb1bf87dfd8fc2c1a62500e
Sandhep <b.sandhep111@gmail.com>
2025-02-18 03:41:46 +0530

    added eway

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 // store/store.js
 import { configureStore } from "@reduxjs/toolkit";
 import  authReducer from '../slices/authSlice.js';
+import  ewayReducer from '../slices/ewaySlice.js';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    eway: ewayReducer
   },
 });
 
