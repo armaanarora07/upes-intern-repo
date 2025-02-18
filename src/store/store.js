31e63bde86075b2a1b21a7e2c05c258ca8d6a0a7
Sandhep <b.sandhep111@gmail.com>
2025-02-18 13:58:19 +0530

    store update

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  authReducer from '../slices/authSlice.js';
 import  ewayReducer from '../slices/ewaySlice.js';
+import businessReducer from '../slices/businessSlice.js';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    eway: ewayReducer
+    eway: ewayReducer,
+    business:businessReducer
   },
 });
 
