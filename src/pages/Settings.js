343d57583fff4232e20f8154ed6aaf9bc2e8148d
Sandhep <b.sandhep111@gmail.com>
2025-02-26 20:05:44 +0530

    added new settings page

---
+++ src/pages/Settings.js
@@ -0,0 +1,33 @@
+import React,{useEffect} from 'react'
+import {useDispatch} from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import Logo from '../components/Logo'
+import { setTitle } from '../slices/navbarSlice';
+
+function Settings() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    dispatch(setTitle('Settings'));
+  })
+    
+  return (
+    <div className='p-8 mt-10'>
+    <div className='p-6 bg-white rounded-lg shadow-xl mt-5'>
+       <Logo/>
+    </div>
+    <div className='p-6 bg-white rounded-lg shadow-xl mt-5'>
+       <h2 className="text-2xl font-bold text-gray-800">Bill Templates</h2>
+    </div>
+    <div className='p-6 bg-white rounded-lg shadow-xl mt-5 cursor-pointer' onClick={()=>{navigate('/add-business')}}>
+       <h2 className="text-2xl font-bold text-gray-800">Add Business</h2>
+    </div>
+    <div className='p-6 bg-white rounded-lg shadow-xl mt-5'>
+       <h2 className="text-2xl font-bold text-gray-800">Enable E-way</h2>
+    </div>
+  </div>
+  )
+}
+
+export default Settings;
